a97a781abb79ab79de7ec7b6a9ba993282b780c5
Erlend Hamnaberg <erlend@hamnaberg.net>
2012-10-19 19:08:49 +0200

    We have extensions

--- src/test/scala/net/hamnaberg/json/collection/extension/ErrorsExtensionSpec.scala
+++ src/test/scala/net/hamnaberg/json/collection/extension/ErrorsExtensionSpec.scala
@@ -10,7 +10,7 @@ class ErrorsExtensionSpec extends Specification {
   "Errors extension " should {
     "be deserialized" in {
       val coll = LiftJsonCollectionParser.parseCollection(getClass.getResourceAsStream("/errors.json")).right
-      val errors = coll.map(_.extract(ErrorsExtension ++ )).toOption.getOrElse(Nil)
+      val errors = coll.map(_.extract(ErrorsExtension)).toOption.getOrElse(Nil)
       errors must be_!=(Nil)
     }
   }
